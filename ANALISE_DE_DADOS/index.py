8ed0871657893b77bf702260f3a7adfbc95912d6
João Dias <joaopedrorochajdias@gmail.com>
2023-06-08 22:49:18 -0300

    Ajuste

---
+++ ANALISE_DE_DADOS/index.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import os
+
+def analiseDeDados():
+            dir = os.getcwd()
+            df = pd.read_excel(rf'{dir}/ANALISE_DE_DADOS/uploads/dados_funcionarios.xlsx')
+            coluna = df['Funcionário']
+            funcionarios = coluna.tolist()
+            coluna = df['Alcance']
+            alcances = coluna.tolist()
+            coluna = df['Meta']
+            metas = coluna.tolist()
+
+            print(funcionarios)
+            print(alcances)
+            print(metas)
+
+analiseDeDados()
